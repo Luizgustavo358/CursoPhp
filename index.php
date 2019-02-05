be33a02a1c31fdeed0631d401a1ddba1a249a414
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2019-02-04 22:16:18 -0200

    estrutura do site, css, favicon

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>WEF Veículos - Anúncio de Veículos em São Paulo</title>
+        <meta name="description" content="A WEF Veículos e uma empresa especializada em anúncios de automóveis e motos na região de São Paulo. Contamos com milhares de veículos a sua disposição.">
+        <meta name="keywords" content="WEF Veículos, empresa de automóveis em SP">
+        <meta name="author" content="Luiz Gustavo">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="images/favicon.ico">
+        <link rel="stylesheet" href="libraries/stylesheet.css">
+    </head>
+
+    <body>
+        <!-- HEADER -->
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Link 2</li>
+                </ul>
+            </nav>
+        </header>
+
+
+        <!-- MAIN -->
+        <main>
+
+        </main>
+
+
+        <!-- FOOTER -->
+        <footer>
+
+        </footer>
+    </body>
+</html>
