cf65e61b862ddd7606449d370ac0509a87eb5ab7
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2019-02-09 16:45:28 -0200

    criando arquivo body que possui elementos que se repetem em todas paginas

---
+++ libraries/body.php
@@ -0,0 +1,28 @@
+<!-- Linha Preta -->
+<div class="ind-topo"></div>
+
+<!-- Links de navegacao -->
+<nav>
+    <ul>
+        <li><a href="index">Home</a></li>
+        <li><a href="sobrenos">Sobre Nós</a></li>
+        <li><a href="anuncios">Anúncios</a></li>
+        <li><a href="contato">Contato</a></li>
+        <li><a href="cadstrese">Cadastre-se</a></li>
+    </ul>
+</nav>
+
+<!-- Botao de Menu Mobile -->
+<div class="ind-botao">
+    <img src="../images/menu-button.png" alt="Botao Mobile" title="Botao Mobile">
+</div>
+
+<!-- Imagens da Logo -->
+<div class="ind-logo">
+    <picture>
+        <source media="(max-width: 480px)" srcset="../images/mobile-logo.png">
+        <source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/mobile-logo.png">
+        <source media="(min-width: 769px)" srcset="../images/desktop-logo.png">
+        <img src="../images/mobile-logo.png" alt="Logomarca WEF Veiculos" title="Logomarca WEF Veiculos">
+    </picture>
+</div>
--- views/index.php
+++ views/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <?php require_once ("../libraries/head.php") ?>
+        <?php require_once ("../libraries/head.php"); ?>
         <title>WEF Veículos - Anúncio de Veículos em São Paulo</title>
         <meta name="description" content="A WEF Veículos e uma empresa especializada em anúncios de automóveis e motos na região de São Paulo. Contamos com milhares de veículos a sua disposição.">
         <meta name="keywords" content="WEF Veículos, empresa de automóveis em SP">
@@ -10,34 +10,7 @@
     <body>
         <!-- HEADER -->
         <header>
-            <!-- Linha Preta -->
-            <div class="ind-topo"></div>
-
-            <!-- Links de navegacao -->
-            <nav>
-                <ul>
-                    <li><a href="index">Home</a></li>
-                    <li><a href="sobrenos">Sobre Nós</a></li>
-                    <li><a href="anuncios">Anúncios</a></li>
-                    <li><a href="contato">Contato</a></li>
-                    <li><a href="cadstrese">Cadastre-se</a></li>
-                </ul>
-            </nav>
-
-            <!-- Botao de Menu Mobile -->
-            <div class="ind-botao">
-                <img src="../images/menu-button.png" alt="Botao Mobile" title="Botao Mobile">
-            </div>
-
-            <!-- Imagens da Logo -->
-            <div class="ind-logo">
-                <picture>
-                    <source media="(max-width: 480px)" srcset="../images/mobile-logo.png">
-                    <source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/mobile-logo.png">
-                    <source media="(min-width: 769px)" srcset="../images/desktop-logo.png">
-                    <img src="../images/mobile-logo.png" alt="Logomarca WEF Veiculos" title="Logomarca WEF Veiculos">
-                </picture>
-            </div>
+            <?php require_once ("../libraries/body.php") ?>
         </header>
 
 
