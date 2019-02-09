78b21417d97656f55a31e5771313ee9047518286
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2019-02-09 18:26:37 -0200

    adicionando slides e imagens

--- libraries/javascript.js
+++ libraries/javascript.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
 
-   // mostra menu
-   $('.ind-botao').on('click', function () {
-       $('nav li').toggle();
-   });
+    // mostra menu
+    $('.ind-botao').on('click', function () {
+        $('nav li').toggle();
+    });
 
+    // muda de pagina
     $('nav li').on('click', function () {
         var link = $(this).children('a').attr('href');
 
         window.location.href = link;
+    });
+
+    // transicao do slide
+    $('.ind-slide').cycle({
+        fx: 'fade'
     });
 });
--- views/index.php
+++ views/index.php
@@ -5,12 +5,38 @@
         <title>WEF Veículos - Anúncio de Veículos em São Paulo</title>
         <meta name="description" content="A WEF Veículos e uma empresa especializada em anúncios de automóveis e motos na região de São Paulo. Contamos com milhares de veículos a sua disposição.">
         <meta name="keywords" content="WEF Veículos, empresa de automóveis em SP">
+        <script src="../libraries/jquery.cycle.lite.js"></script>
     </head>
 
     <body>
         <!-- HEADER -->
         <header>
             <?php require_once ("../libraries/body.php") ?>
+            <div class="ind-slide">
+                <!-- Slide 1 -->
+                <picture>
+                    <source media="(max-width: 480px)" srcset="../images/mobile-slide1.jpg">
+                    <source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/tablet-slide1.jpg">
+                    <source media="(min-width: 769px)" srcset="../images/desktop-slide1.jpg">
+                    <img src="../images/mobile-slide1.jpg" alt="Veiculos WEF - Slide 1" title="Veiculos WEF - Slide 1">
+                </picture>
+
+                <!-- Slide 2 -->
+                <picture>
+                    <source media="(max-width: 480px)" srcset="../images/mobile-slide2.jpg">
+                    <source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/tablet-slide2.jpg">
+                    <source media="(min-width: 769px)" srcset="../images/desktop-slide2.jpg">
+                    <img src="../images/mobile-slide2.jpg" alt="Veiculos WEF - Slide 2" title="Veiculos WEF - Slide 2">
+                </picture>
+
+                <!-- Slide 3 -->
+                <picture>
+                    <source media="(max-width: 480px)" srcset="../images/mobile-slide3.jpg">
+                    <source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/tablet-slide3.jpg">
+                    <source media="(min-width: 769px)" srcset="../images/desktop-slide3.jpg">
+                    <img src="../images/mobile-slide3.jpg" alt="Veiculos WEF - Slide 3" title="Veiculos WEF - Slide 3">
+                </picture>
+            </div>
         </header>
 
 
