f413017c0bd765f5c3281d11a77e87b37a0ce307
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2019-02-09 16:15:52 -0200

    Menu responsivo, criaçao de regras de url amigavel e redirecionamento

--- libraries/javascript.js
+++ libraries/javascript.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
    // mostra menu
    $('.ind-botao').on('click', function () {
        $('nav li').toggle();
-   })
+   });
 
+    $('nav li').on('click', function () {
+        var link = $(this).children('a').attr('href');
+
+        window.location.href = link;
+    });
 });
---
+++ views/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>WEF Veículos - Anúncio de Veículos em São Paulo</title>
+        <meta name="description" content="A WEF Veículos e uma empresa especializada em anúncios de automóveis e motos na região de São Paulo. Contamos com milhares de veículos a sua disposição.">
+        <meta name="keywords" content="WEF Veículos, empresa de automóveis em SP">
+        <meta name="author" content="Luiz Gustavo">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="../images/favicon.ico">
+        <link rel="stylesheet" href="../libraries/stylesheet.css">
+        <script src="../libraries/jquery-3.3.1.min.js"></script>
+        <script src="../libraries/javascript.js"></script>
+    </head>
+
+    <body>
+        <!-- HEADER -->
+        <header>
+            <!-- Linha Preta -->
+            <div class="ind-topo"></div>
+
+            <!-- Links de navegacao -->
+            <nav>
+                <ul>
+                    <li><a href="index">Home</a></li>
+                    <li><a href="sobrenos">Sobre Nós</a></li>
+                    <li><a href="anuncios">Anúncios</a></li>
+                    <li><a href="contato">Contato</a></li>
+                    <li><a href="cadstrese">Cadastre-se</a></li>
+                </ul>
+            </nav>
+
+            <!-- Botao de Menu Mobile -->
+            <div class="ind-botao">
+                <img src="../images/menu-button.png" alt="Botao Mobile" title="Botao Mobile">
+            </div>
+
+            <!-- Imagens da Logo -->
+            <div class="ind-logo">
+                <picture>
+                    <source media="(max-width: 480px)" srcset="../images/mobile-logo.png">
+                    <source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/mobile-logo.png">
+                    <source media="(min-width: 769px)" srcset="../images/desktop-logo.png">
+                    <img src="../images/mobile-logo.png" alt="Logomarca WEF Veiculos" title="Logomarca WEF Veiculos">
+                </picture>
+            </div>
+        </header>
+
+
+        <!-- MAIN -->
+        <main>
+
+        </main>
+
+
+        <!-- FOOTER -->
+        <footer>
+
+        </footer>
+    </body>
+</html>
